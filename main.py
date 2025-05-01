c9d18c7856b9888fe1fd5a45196634115a8427ef
Yousef Amirghofran <yamirghofran@gmail.com>
2025-05-01 23:41:04 +0200

    Comment out sends

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import os
 if __name__ == "__main__":
     bot = SlackBot()
     handler = SocketModeHandler(bot.app, os.environ["SLACK_APP_TOKEN"]) # Connecting to Slack via HTTP endpoint in case u want to listen for events
-    bot.send_message(":wave: Hello Catherineeee!", channel="#general")
-    bot.send_file("test.jpg", channel="#general")
-    bot.send_file("test.csv", channel="#general")
+    #bot.send_message(":wave: Hello Catherineeee!", channel="#general")
+    #bot.send_file("test.jpg", channel="#general")
+    #bot.send_file("test.csv", channel="#general")
     handler.start()
